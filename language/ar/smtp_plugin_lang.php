fffa7defd4ab9ee75f7627c36f9d66a612deca06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 06:13:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/smtp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['smtp_plugin_app_name'] = 'سياسات خادم SMTP';
+$lang['smtp_plugin_app_description'] = 'توفر سياسات خادم SMTP التحكم في الوصول لهذا التطبيق خادم SMTP.';
+$lang['smtp_plugin_smtp_server_user'] = 'SMTP Server User';
