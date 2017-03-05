c882ebd8d98cd4ca10c3acda3828db6f96b9737f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:29:32 +0000

    Add new files and directories and commit changes to local repository

--- language/id/smtp_plugin_lang.php
+++ language/id/smtp_plugin_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['smtp_plugin_app_name'] = 'SMTP Server Policies';
-$lang['smtp_plugin_app_description'] = 'SMTP Server Policies provide access control for the SMTP Server app.';
-$lang['smtp_plugin_smtp_server_user'] = 'SMTP Server User';
+$lang['smtp_plugin_app_name'] = 'Kebijakan SMTP Server';
+$lang['smtp_plugin_app_description'] = 'SMTP Kebijakan Server menyediakan kontrol akses untuk aplikasi SMTP Server.';
+$lang['smtp_plugin_smtp_server_user'] = 'SMTP Server Pengguna';
