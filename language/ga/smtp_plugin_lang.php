09575be1a49cb830936c52fb4faecee67d46267e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:29:41 +0000

    Add new files and directories and commit changes to local repository

--- language/ga/smtp_plugin_lang.php
+++ language/ga/smtp_plugin_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['smtp_plugin_app_name'] = 'SMTP Server Policies';
-$lang['smtp_plugin_app_description'] = 'SMTP Server Policies provide access control for the SMTP Server app.';
-$lang['smtp_plugin_smtp_server_user'] = 'SMTP Server User';
+$lang['smtp_plugin_app_name'] = 'Polasaithe SMTP Freastalaí';
+$lang['smtp_plugin_app_description'] = 'a chur ar fáil Polasaithe Freastalaí SMTP rialaithe rochtana don app SMTP Freastalaí.';
+$lang['smtp_plugin_smtp_server_user'] = 'SMTP Freastalaí Úsáideora';
