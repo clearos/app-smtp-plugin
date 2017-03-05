5580f6ba12b076b9b031a5fa6a276c4a966b85e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:32:10 +0000

    Add new files and directories and commit changes to local repository

--- language/mi/smtp_plugin_lang.php
+++ language/mi/smtp_plugin_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['smtp_plugin_app_name'] = 'SMTP Server Policies';
-$lang['smtp_plugin_app_description'] = 'SMTP Server Policies provide access control for the SMTP Server app.';
-$lang['smtp_plugin_smtp_server_user'] = 'SMTP Server User';
+$lang['smtp_plugin_app_name'] = 'Kaupapa here Tūmau SMTP';
+$lang['smtp_plugin_app_description'] = 'whakarato kaupapa here Tūmau SMTP mana uru mo te taupānga Tūmau SMTP.';
+$lang['smtp_plugin_smtp_server_user'] = 'Kaiwhakamahi Tūmau SMTP';
