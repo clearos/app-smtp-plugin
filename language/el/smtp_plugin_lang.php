d742c3204e3b330cdea3bec09d54ae43c537c2bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:28:33 +0000

    Add new files and directories and commit changes to local repository

--- language/el/smtp_plugin_lang.php
+++ language/el/smtp_plugin_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['smtp_plugin_app_name'] = 'SMTP Server Policies';
-$lang['smtp_plugin_app_description'] = 'SMTP Server Policies provide access control for the SMTP Server app.';
-$lang['smtp_plugin_smtp_server_user'] = 'SMTP Server User';
+$lang['smtp_plugin_app_name'] = 'Πολιτικές SMTP διακομιστή';
+$lang['smtp_plugin_app_description'] = 'SMTP των Server παρέχει έλεγχο πρόσβασης για την εφαρμογή SMTP Server.';
+$lang['smtp_plugin_smtp_server_user'] = 'SMTP Server χρήστη';
