45a124f8b701c340437adf7eaf89d57df68cd104
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:26:45 +0000

    Add new files and directories and commit changes to local repository

--- language/ny/smtp_plugin_lang.php
+++ language/ny/smtp_plugin_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['smtp_plugin_app_name'] = 'SMTP Server Policies';
-$lang['smtp_plugin_app_description'] = 'SMTP Server Policies provide access control for the SMTP Server app.';
-$lang['smtp_plugin_smtp_server_user'] = 'SMTP Server User';
+$lang['smtp_plugin_app_name'] = 'Ndondomeko SMTP Server';
+$lang['smtp_plugin_app_description'] = 'SMTP Ndondomeko Server imapatsa kulamulira kwa app SMTP Server.';
+$lang['smtp_plugin_smtp_server_user'] = 'SMTP Server Wosuta';
