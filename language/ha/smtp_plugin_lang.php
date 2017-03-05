6788724782ef89630ff793e0f4a3d29286a7ff2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:29:03 +0000

    Add new files and directories and commit changes to local repository

--- language/ha/smtp_plugin_lang.php
+++ language/ha/smtp_plugin_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['smtp_plugin_app_name'] = 'SMTP Server Policies';
-$lang['smtp_plugin_app_description'] = 'SMTP Server Policies provide access control for the SMTP Server app.';
+$lang['smtp_plugin_app_name'] = 'SMTP Server Asiri';
+$lang['smtp_plugin_app_description'] = 'SMTP Server Asiri samar da damar iko da SMTP Server app.';
 $lang['smtp_plugin_smtp_server_user'] = 'SMTP Server User';
